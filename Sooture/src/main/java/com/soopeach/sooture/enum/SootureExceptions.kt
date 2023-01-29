d96de317eb39f8e8d55bccf32d36741d21a95a6d
soopeach <hsjeon584@gmail.com>
2023-01-29 16:50:38 +0900

    Feat: processing for avoid duplicated file name

--- Sooture/src/main/java/com/soopeach/sooture/enum/SootureExceptions.kt
+++ Sooture/src/main/java/com/soopeach/sooture/enum/SootureExceptions.kt
@@ -7,7 +7,8 @@ package com.soopeach.sooture.enum
  *
  */
 internal enum class SootureExceptions(val exception: Exception) {
-    INVALID_VIEW(Exception("Invalid View"))
+    INVALID_VIEW(Exception("Invalid View")),
+    INVALID_DATE(Exception("Invalid Date"))
 }
 
 
